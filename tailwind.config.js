2c5706f7ce8283020906f362b0e0b3e310657e96
Carla Valdivia <valdivia.carla@gmail.com>
2024-09-18 16:59:36 -0300

    Changed dark mode to light mode design

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,14 @@ export default {
         sandwich: "0 8px, 0px -8px ",
       },
       colors: {
-        background: '#210A2E',
-        lPurple: '#B868FF',
-        lGray: '#d1cfcf',
-        mGray: '#9888B0',
-        dGray: '#140d42'
+        background: '#FFF',
+        lPurple: '#D9CCFE',
+        mPurple: '#BCA6FE',
+        dPurple: '#835AFD',
+        lGray: '#EEEDF1',
+        mGray: '#C5C2D0',
+        dGray: '#6E6A78', 
+        DGray: '#403E46'
       },
     },
   },
