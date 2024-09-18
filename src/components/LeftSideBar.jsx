776202f755c2c747099952c4536b2968e2f86ee8
Carla Valdivia <valdivia.carla@gmail.com>
2024-09-18 17:57:11 -0300

    Add target blank + noreferrer to leftsidebar links

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -1,19 +1,19 @@
 export default function LeftSideBar() {
   return (
     <div className="left_sidebar">
-      <a href="https://github.com/cvaldivia83">
+      <a href="https://github.com/cvaldivia83" target="_blank" rel="noreferrer">
         <i className="fa-brands fa-github"></i>
       </a>
 
-      <a href="https://www.instagram.com/cvaldivia83/">
+      <a href="https://www.instagram.com/cvaldivia83/" target="_blank" rel="noreferrer">
         <i className="fa-brands fa-instagram"></i>
       </a>
 
-      <a href="https://www.linkedin.com/in/carla-cabrera-valdivia/">
+      <a href="https://www.linkedin.com/in/carla-cabrera-valdivia/" target="_blank" rel="noreferrer">
         <i className="fa-brands fa-linkedin"></i>
       </a>
 
-      <a href="https://wa.me/5521974816078">
+      <a href="https://wa.me/5521974816078" target="_blank" rel="noreferrer">
         <i className="fa-brands fa-whatsapp"></i>
       </a>
     </div>
