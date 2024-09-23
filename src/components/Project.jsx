e10a08329313572280104889ab478599711478e3
Carla Valdivia <valdivia.carla@gmail.com>
2024-09-23 01:07:39 -0300

    Add projects component + css

---
+++ src/components/Project.jsx
@@ -0,0 +1,9 @@
+import SectionTitle from './SectionTitle';
+
+export default function Project() {
+  return (
+    <div className="project" id="work">
+      <SectionTitle title="Some Things I've Build" order="section-03" />
+    </div>
+  )
+}
