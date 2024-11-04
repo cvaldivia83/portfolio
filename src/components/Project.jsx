11641e66e230a310c90536a6b30ea3991bb9a1b9
Carla Valdivia <valdivia.carla@gmail.com>
2024-11-04 18:40:17 -0300

    Change animation bottom-up on hero component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,14 @@
+import React from 'react';
 import SectionTitle from './SectionTitle';
 
 export default function Project() {
   return (
-    <div className="project" id="work">
+    <div className="projects" id="work">
       <SectionTitle title="Some Things I've Build" order="section-03" />
+
+      <div className="project">
+        <img src="https://res.cloudinary.com/rent-an-instrument/image/upload/v1730755071/Screenshot_2024-11-04_at_14.24.40_bozfen.png" alt="" />
+      </div>
     </div>
   )
 }
