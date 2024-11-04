52ca8e4053fc90184e3be754b7a69b3581adda53
Carla Valdivia <valdivia.carla@gmail.com>
2024-11-04 20:16:22 -0300

    Add useScroll hook to project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import SectionTitle from './SectionTitle';
+import useScroll from '../hooks/useScroll';
 
 export default function Project() {
+  const projectSection = React.useRef();
+  const isVisible = useScroll(projectSection);
+
   return (
-    <div className="projects" id="work">
+    <div className={`projects ${isVisible && 'visible'}`} id="work" ref={projectSection}>
       <SectionTitle title="Some Things I've Build" order="section-03" />
 
       <div className="project">
