f2f3092b569f4ff1063f3d55a8c6609b3ed39812
Carla Valdivia <valdivia.carla@gmail.com>
2024-11-12 20:20:37 -0300

    Fix useMedia hook

--- src/hooks/useMedia.jsx
+++ src/hooks/useMedia.jsx
@@ -4,10 +4,13 @@ const useMedia = (media) => {
   const [match, setMatch] = React.useState(null);
 
   React.useEffect(() => {
+    
     function changeMatch() {
       const { matches } = window.matchMedia(media);
       setMatch(matches);
     }
+
+    changeMatch();
 
     window.addEventListener("resize", changeMatch);
     return () => {
