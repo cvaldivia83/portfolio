1c44b08cf0ff324f3eda26bf85524c953d01ae8e
Carla Valdivia <valdivia.carla@gmail.com>
2024-11-18 18:28:27 -0300

    Fix navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,27 +5,17 @@ export default function Navbar() {
   const mobile = useMedia('(max-width: 40rem)');
   const [mobileMenu, setMobileMenu] = React.useState(false);
   // check if sandwich menu is open or closed
-  const [isVisible, setIsVisible] = React.useState(false);
-
-
-  function handleScroll(e, sectionId) {
-    e.preventDefault();
-    const item = document.getElementById(sectionId);
-    if (item) {
-      item.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
 
   function handleClick() {
-    console.log(mobileMenu);
-    setMobileMenu(!mobileMenu)
-    setIsVisible(!isVisible);
+    setMobileMenu(!mobileMenu);
   }
-
   
   return (
-    <>
-      
+    <div className="nav-container">
+      <div className={ mobile ? 'hidden' : 'nav-logo'}>
+        <i className="fa-solid fa-code"></i>
+      </div>
+
       { mobile && (
         <button 
         onClick={handleClick}
@@ -35,37 +25,26 @@ export default function Navbar() {
         </button>
         )
       }
-      
 
-    <div className="flex justify-between items-center pl-8">
-      <div className={`${mobile ? "hidden" : "navbar-logo"}`}>
-          <i className="fa-solid fa-code"></i>
-      </div>
-      {/*  */}
-      <nav className={`navbar ${mobile ? 'navMobile' : 'navbar'} ${mobileMenu && 'navMobileActive'} ${isVisible ? 'visible' : ''}`}>
-        <ul className="navbar-list">
-          <li className="navbar-item item-counter">
-            <a href="#about" onClick={(e) => handleScroll(e, 'about')}>About</a>
+      <nav className={ `${mobile ? 'navMobile' : 'nav'} ${ mobileMenu && 'navMobileActive' }` }>
+        <ul className="nav-list">
+          <li className="nav-item item-counter">
+            <a href="#about" className="href">About</a>
           </li>
-          <li className="navbar-item item-counter">
-            <a href="#experience" onClick={(e) => handleScroll(e, 'experience')}>Experience</a>
+          <li className="nav-item item-counter">
+            <a href="#experience">Experience</a>
           </li>
-          <li className="navbar-item item-counter">
-            <a href="#work" onClick={(e) => handleScroll(e, 'work')}>Work</a>
+          <li className="nav-item item-counter">
+            <a href="#work">Work</a>
           </li>
-          <li className="navbar-item item-counter">
-            <a href="#contact" onClick={(e) => handleScroll(e, 'contact')}>Contact</a>
+          <li className="nav-item item-counter">
+            <a href="#contact">Contact</a>
           </li>
-          <li className={`${mobile ? "navbar-item item-counter" : "resume-btn"}`}>
+          <li className={ mobile ? 'nav-item item-counter' : 'resume-btn'}>
             <a href="/cacv.pdf"  target="_blank">Resume</a>
           </li>
         </ul>
       </nav>
-
-
     </div>
-
-
-    </>
   );
 }
